713e909bcf895f6bfd5df6909e415d2413551828
Javier Infante <jabi@irontec.com>
2019-01-17 09:32:42 +0100

    Updates

---
+++ precache-manifest.f80f73a49f287e064fe427a09b381c8a.js
@@ -0,0 +1,95 @@
+self.__precacheManifest = [
+  {
+    "revision": "560b4822cf85f6084642",
+    "url": "app.css"
+  },
+  {
+    "revision": "560b4822cf85f6084642",
+    "url": "app.js"
+  },
+  {
+    "revision": "78d0500de668984165dd",
+    "url": "hljs-langs/javascript.js"
+  },
+  {
+    "revision": "a08cbcfe6cda0789f9cc",
+    "url": "hljs-langs/typescript.js"
+  },
+  {
+    "revision": "9d9be852aec6202f99a33cf9cc292d9c",
+    "url": "95c7b5a26ab51d96ee39f36c66005d67.svg"
+  },
+  {
+    "revision": "5d039d9f3408836ef24841905f04b69b",
+    "url": "1ba934df20c60bfbda92ae00e4ac1b37.svg"
+  },
+  {
+    "revision": "8a63aaf795c65b9a001dfc6bf7387293",
+    "url": "66cec3167484d42c1ccc513c6259a4f8.svg"
+  },
+  {
+    "revision": "6e29ff6d4e03bd5ba8b3d5556174789e",
+    "url": "b97c530867a61ea60506b10a416762c4.svg"
+  },
+  {
+    "revision": "e688707ed86fca73e90fd1e2d3dc4645",
+    "url": "a0296bb86117cab30ef27fd515a87f79.svg"
+  },
+  {
+    "revision": "43b843880f019c3bdf2a047b201961a7",
+    "url": "187a99d3fe4e62c75737c5f4a93e00a6.svg"
+  },
+  {
+    "url": "fc84e998bc29b297ea20321e4c90b6ed.ttf"
+  },
+  {
+    "url": "3e1af3ef546b9e6ecef9f3ba197bf7d2.ttf"
+  },
+  {
+    "url": "89e2666c24d37055bcb60e9d2d9f7e35.ttf"
+  },
+  {
+    "url": "ee7b96fa85d8fdb8c126409326ac2d2b.ttf"
+  },
+  {
+    "url": "0fc25386220a58203994ce45fb4ae570.ttf"
+  },
+  {
+    "url": "d1efcd4d126837fe0dcf9b6cf3a00d64.ttf"
+  },
+  {
+    "url": "42bbe4eefcde1297b11dc4b6491e9746.ttf"
+  },
+  {
+    "url": "ec4c9962ba54eb91787aa93d361c10a8.ttf"
+  },
+  {
+    "url": "50705c5ed1205b63efdbfee941a6b655.ttf"
+  },
+  {
+    "url": "1eb7a893589ddce89d81cdb22a356660.ttf"
+  },
+  {
+    "url": "2248635fc70a2b049779fadea95c82ad.jpg"
+  },
+  {
+    "revision": "8ec6268e47b6b2ca18a0b054471be6b4",
+    "url": "3rd.party.LICENSES"
+  },
+  {
+    "revision": "c5e74152a707a5ba2ef642c9aca2b904",
+    "url": "media/logo_irontec.png"
+  },
+  {
+    "revision": "22808ca68f8e4649570b2f4152b45be2",
+    "url": "media/images/logo.png"
+  },
+  {
+    "revision": "e0ab72b5ae2614809f4620082714ffde",
+    "url": "index.html"
+  },
+  {
+    "revision": "27ddf47c7a7de1b45858d6d59a4017f5",
+    "url": "notes.html"
+  }
+];
